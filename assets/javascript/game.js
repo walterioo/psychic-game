26f7fd8f51e2b2ca5c27af489ffeb3758e5a4011
Walter A <39980099+walterioo@users.noreply.github.com>
2018-09-03 13:36:18 -0700

    fixed guesses, no more counting 0 as a guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
  // Array with all alphabet letters
  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ñ"];
+     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ñ"
+ ];
 
 
  // Variables to track scores
@@ -12,43 +13,40 @@
 
  // JS is waiting for a keyup to star the fuction event 
  document.onkeyup = function (event) {
-    // Assigns the key value to userGuess
-    var userGuess = event.key.toLowerCase();
-    // Variable to randomize the letters array
-    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+     // Variable to randomize the letters array
+     var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
-    // The game will alert no more guesses are left when it gets to 0
-    if(guessesLeft > 0){
-        loss ++;
-        guessesLeft = 10;
-        userLetters = [];
-    }
-    else { 
-        
-        // Nested if to compare user choice vs computer
-        if (userGuess === computerChoice) {
-            wins++;
-            guessesLeft = 10;
-            userLetters = [];
-        } 
-        else {
-            guessesLeft--;
-        }
-    }
-    
-    // This if will prevent a letter to be written multiple times 
-    if(userLetters.indexOf(userGuess) >=0){
+     // Assigns the key value to userGuess
+     var userGuess = event.key.toLowerCase();
 
-    }
-    else {
-        userLetters.push(userGuess);
-        document.querySelector("#user-letters").innerHTML = userLetters ;
-    }
-    
+     // If the letter is the same as the random it will add 1 win and resset guesses and clear array
+     if (userGuess === computerChoice) {
+         wins++;
+         guessesLeft = 10;
+         userLetters = [];
+         //If not will substract 1 guess    
+     } else {
+         guessesLeft--;
+     }
+     // Check gueeses left, if equals to 0 it will add 1 loss and reset guesses left and clear the user letters array
+     if (guessesLeft === 0) {
+         loss++;
+         guessesLeft = 10;
+         userLetters = [];
+     }
 
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#loss").innerHTML = loss;
-    document.querySelector("#guess").innerHTML = guessesLeft;
-    
+     // This if will prevent a letter to be written multiple times 
+     if (userLetters.indexOf(userGuess) >= 0) {
+
+     } else {
+         userLetters.push(userGuess);
+         document.querySelector("#user-letters").innerHTML = userLetters;
+     }
+
+
+     document.querySelector("#wins").innerHTML = wins;
+     document.querySelector("#loss").innerHTML = loss;
+     document.querySelector("#guess").innerHTML = guessesLeft;
+
 
  }
